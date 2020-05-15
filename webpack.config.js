5dccb289afe37d631c0502a7cf596f845582fe0d
Oleg Luzhnyak <luzhnyak@yandex.ru>
2020-05-15 14:04:01 +0300

    Initial comit

---
+++ webpack.config.js
@@ -0,0 +1,46 @@
+const path = require('path');
+const { EnvironmentPlugin } = require('webpack');
+const HTMLPlugin = require('html-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.[chunkhash].js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    port: 3000
+  },
+  plugins: [
+    new HTMLPlugin({
+      template: './src/index.html'
+    }),
+    new CleanWebpackPlugin(),
+    new Dotenv(),
+    new EnvironmentPlugin(['API_URL'])
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              configFile: "./babel.config.js",
+              cacheDirectory: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  devtool: "source-map"
+};
